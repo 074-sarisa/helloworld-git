b989c6195b79561384e5f2ea5c2d28ef1fc2e305
amm <sarisaza@gmail.com>
2026-03-14 14:55:38 +0700

    Add Day2

---
+++ test_tailwindcss/src/components/Project.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <div class="grid grid-cols-3 gap-6 border-4 border-black min-h-80 bg-red-400 rounded-2xl m-50 mt-50">
+        <h1>HELLO WORLD</h1>
+        <p class="text-blue-600 text-6xl bg-pink-200">Ittiched</p>
+        <p class="text-blue-600 text-6xl bg-pink-200 col-span-2">Ittiched</p>
+        <p class="text-blue-600 text-6xl bg-pink-200">Ittiched</p>
+        <p class="text-blue-600 text-6xl bg-pink-200">Ittiched</p>
+    </div>
+</template>
+
+<style scoped></style>
